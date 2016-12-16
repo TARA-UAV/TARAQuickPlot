eee017318506781065a03a36c6e56f6a48edc057
TARA-UAV <tara.uav@gmail.com>
2016-12-15 21:12:28 -0500

    First commit

---
+++ TaraQuickPlot/frmMain.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim ChartArea1 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea
+        Dim Legend1 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend
+        Dim Series1 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series
+        Me.chtPosition = New System.Windows.Forms.DataVisualization.Charting.Chart
+        Me.cmdPlotPosition = New System.Windows.Forms.Button
+        CType(Me.chtPosition, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'chtPosition
+        '
+        ChartArea1.Name = "ChartArea1"
+        Me.chtPosition.ChartAreas.Add(ChartArea1)
+        Legend1.Name = "Legend1"
+        Me.chtPosition.Legends.Add(Legend1)
+        Me.chtPosition.Location = New System.Drawing.Point(24, 12)
+        Me.chtPosition.Name = "chtPosition"
+        Series1.ChartArea = "ChartArea1"
+        Series1.Legend = "Legend1"
+        Series1.Name = "Series1"
+        Me.chtPosition.Series.Add(Series1)
+        Me.chtPosition.Size = New System.Drawing.Size(730, 300)
+        Me.chtPosition.TabIndex = 0
+        Me.chtPosition.Text = "Position Data"
+        '
+        'cmdPlotPosition
+        '
+        Me.cmdPlotPosition.Location = New System.Drawing.Point(358, 331)
+        Me.cmdPlotPosition.Name = "cmdPlotPosition"
+        Me.cmdPlotPosition.Size = New System.Drawing.Size(128, 23)
+        Me.cmdPlotPosition.TabIndex = 1
+        Me.cmdPlotPosition.Text = "Plot Position Data"
+        Me.cmdPlotPosition.UseVisualStyleBackColor = True
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(766, 366)
+        Me.Controls.Add(Me.cmdPlotPosition)
+        Me.Controls.Add(Me.chtPosition)
+        Me.Name = "frmMain"
+        Me.Text = "TARA Quick Plot"
+        CType(Me.chtPosition, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents chtPosition As System.Windows.Forms.DataVisualization.Charting.Chart
+    Friend WithEvents cmdPlotPosition As System.Windows.Forms.Button
+
+End Class
---
+++ TaraQuickPlot/frmMain.vb
@@ -0,0 +1,80 @@
+﻿Option Explicit On
+Imports System.IO
+Public Class frmMain
+
+    Dim srsBack As New DataVisualization.Charting.Series
+    Dim srsRight As New DataVisualization.Charting.Series
+    Dim srsAltitude As New DataVisualization.Charting.Series
+
+
+    Private Sub frmMain_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        chtPosition.Series.Clear()
+        chtPosition.Titles.Add("Sonar range")
+        chtPosition.ChartAreas(0).AxisX.ScrollBar.Enabled = True
+        chtPosition.ChartAreas(0).AxisX.Title = "Time [0.002 sec]" 'Chart1.ChartAreas("ChartArea1").AxisX.Title = "Time"
+        chtPosition.ChartAreas(0).AxisY.Title = "Position [cm]"
+        chtPosition.ChartAreas(0).AxisX.ScaleView.Size = 100
+
+        srsBack.Name = "Back"
+        srsBack.ChartType = DataVisualization.Charting.SeriesChartType.FastLine
+        srsRight.Name = "Right"
+        srsRight.ChartType = DataVisualization.Charting.SeriesChartType.FastLine
+        srsAltitude.Name = "Altitude"
+        srsAltitude.ChartType = DataVisualization.Charting.SeriesChartType.FastLine
+
+        chtPosition.Series.Add(srsBack)
+        chtPosition.Series.Add(srsRight)
+        chtPosition.Series.Add(srsAltitude)
+
+        chtPosition.Series("Back").Color = Color.Blue
+        chtPosition.Series("Right").Color = Color.Red
+        chtPosition.Series("Altitude").Color = Color.Black
+
+        chtPosition.Series("Back").BorderWidth = 3
+        chtPosition.Series("Right").BorderWidth = 3
+        chtPosition.Series("Altitude").BorderWidth = 3
+
+    End Sub
+
+    Private Sub cmdPlotPosition_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdPlotPosition.Click
+        Dim openFile As New OpenFileDialog()
+        Dim strIncoming As String
+        Try
+            openFile.Filter = "Text File|*.txt"
+            openFile.Title = "Open Log File"
+
+            ' If the file name is not an empty string save the name.
+            If openFile.ShowDialog() = DialogResult.OK Then
+                Dim srRead = New StreamReader(openFile.FileName, True)
+                'TargetFile.WriteLine(())
+
+                While Not srRead.ReadToEnd()
+                    strIncoming = srRead.ReadLine()
+                    strIncoming = strIncoming.Trim()
+
+                    Dim strArray() As String = Split(strIncoming, ",")
+
+                    Select Case strArray(0)
+                        Case "Xaxis"
+                            srsBack.Points.AddY(CSng(strArray(5)) / 10.0)
+
+                        Case "Yaxis"
+                            srsRight.Points.AddY(CSng(strArray(5)) / 10.0)
+                            'srsRight.Points.AddXY(dblLastTime, CDbl(strArray(5)) / 10.0)
+                        Case "Zaxis"
+                            srsAltitude.Points.AddY(CSng(strArray(5)) / 10.0)
+                            'srsAltitude.Points.AddXY(dblLastTime, CDbl(strArray(5)) / 10.0)
+
+                    End Select
+
+                End While
+                srRead.Close()
+
+
+            End If
+
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+End Class
